870a4e6cd21aa48a44dd2da44e4bb7381e09ddf0
Rodriguezz <rodriguezz4060@gmail.com>
2022-03-29 18:39:28 +0300

    использование функции вывода/удаления первого значения массива .shift()

---
+++ JavaScript/basic_JavaScript/Manipulate_Arrays_With_shift().js
@@ -0,0 +1,9 @@
+/*
+Use the .shift() function to remove the first item from myArray and assign
+the "shifted off" value to a new variable, removedFromMyArray.*/
+
+// Налаштування
+const myArray = [["John", 23], ["dog", 3]];
+
+// Змініть код лише під цим рядком
+const removedFromMyArray = myArray.shift();
