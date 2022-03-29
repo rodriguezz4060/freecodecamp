75f2d03243bae11793ea45bdf43798c244fa6a0f
Rodriguezz <rodriguezz4060@gmail.com>
2022-03-29 12:30:58 +0300

    использование экранирования

---
+++ basic_JavaScript/Escape_Sequences_in_Strings.js
@@ -0,0 +1,7 @@
+/* Assign the following three lines of text into the single variable myStr using escape sequences.
+
+    FirstLine
+        \SecondLine
+    ThirdLine */
+
+const myStr = "FirstLine\n\t\\SecondLine\nThirdLine"; // Змініть цей рядок
