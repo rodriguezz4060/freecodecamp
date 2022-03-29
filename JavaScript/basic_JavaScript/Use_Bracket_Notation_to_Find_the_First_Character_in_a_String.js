1c22ace5358bc2e853b3714987487fa0a9c2c087
Rodriguezz <rodriguezz4060@gmail.com>
2022-03-29 13:37:59 +0300

    использование скобковой нотации [] для получения конкретного символа в переменной

---
+++ JavaScript/basic_JavaScript/Use_Bracket_Notation_to_Find_the_First_Character_in_a_String.js
@@ -0,0 +1,12 @@
+/*
+Use bracket notation to find the first character in the lastName
+variable and assign it to firstLetterOfLastName.
+Hint: Try looking at the example above if you get stuck.
+*/
+
+// Налаштування
+let firstLetterOfLastName = "";
+const lastName = "Lovelace";
+
+// Змініть код лише під цим рядком
+firstLetterOfLastName = lastName[0]; // Змініть цей рядок
