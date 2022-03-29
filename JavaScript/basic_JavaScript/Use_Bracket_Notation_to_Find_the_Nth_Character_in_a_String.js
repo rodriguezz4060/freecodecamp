e84b4256f6b64fd04fc80e8558268639f8007b95
Rodriguezz <rodriguezz4060@gmail.com>
2022-03-29 13:48:32 +0300

    использование скобочной нотации для получениясимвола

---
+++ JavaScript/basic_JavaScript/Use_Bracket_Notation_to_Find_the_Nth_Character_in_a_String.js
@@ -0,0 +1,12 @@
+/*
+Let's try to set thirdLetterOfLastName to equal the third letter of the ' +
+'lastName variable using bracket notation.
+
+Hint: Try looking at the example above if you get stuck.
+*/
+
+// Налаштування
+const lastName = "Lovelace";
+
+// Змініть код лише під цим рядком
+const thirdLetterOfLastName = lastName[2]; // Змініть цей рядок
