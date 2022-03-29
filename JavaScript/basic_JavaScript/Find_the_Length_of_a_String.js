34299b5adbaee722781b43f6d8cdab6874826c45
Rodriguezz <rodriguezz4060@gmail.com>
2022-03-29 13:33:52 +0300

    использование оператора .length

---
+++ JavaScript/basic_JavaScript/Find_the_Length_of_a_String.js
@@ -0,0 +1,10 @@
+/*
+Use the .length property to count the number of characters in the lastName
+variable and assign it to lastNameLength.*/
+
+// Налаштування
+let lastNameLength = 0;
+const lastName = "Lovelace";
+
+// Змініть код лише під цим рядком
+lastNameLength = lastName.length;
