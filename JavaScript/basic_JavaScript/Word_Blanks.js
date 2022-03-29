ea429b469a275f01ef78c00c6d06ab6cd41683fa
Rodriguezz <rodriguezz4060@gmail.com>
2022-03-29 14:19:28 +0300

    обьеденение переменных в одно предложение

---
+++ JavaScript/basic_JavaScript/Word_Blanks.js
@@ -0,0 +1,14 @@
+/*
+You will need to use the string concatenation operator + to build a new string,
+using the provided variables: myNoun, myAdjective, myVerb, and myAdverb.
+You will then assign the formed string to the wordBlanks variable.
+You should not change the words assigned to the variables.*/
+
+const myNoun = "dog";
+const myAdjective = "big";
+const myVerb = "ran";
+const myAdverb = "quickly";
+
+// Змініть код лише під цим рядком
+const wordBlanks = "The " + myAdjective + " " + myNoun + " " + myVerb  + " " + myAdverb + "."; // Змініть цей рядок
+// Змініть код лише над цим рядком
