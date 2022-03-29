079308194fc4385dd969af020d805725bf266533
Rodriguezz <rodriguezz4060@gmail.com>
2022-03-29 13:45:36 +0300

    замена данных в переменной

---
+++ JavaScript/basic_JavaScript/Understand_String_Immutability.js
@@ -0,0 +1,12 @@
+/*
+Correct the assignment to myStr so it contains the string
+value of Hello World using the approach shown in the example above.
+myStr should have a value of the string Hello World.
+You should not change the code above the specified comment.*/
+
+// Налаштування
+let myStr = "Jello World";
+
+// Змініть код лише під цим рядком
+myStr = "Hello World"; // Змініть цей рядок
+// Змініть код лише над цим рядком
