a4e30a72ab767bc204e31a3058c4ec1af417a19f
Rodriguezz <rodriguezz4060@gmail.com>
2022-03-29 18:21:50 +0300

    использование функции .push()

---
+++ JavaScript/basic_JavaScript/Manipulate_Arrays_With_push().js
@@ -0,0 +1,10 @@
+/*
+Push ["dog", 3] onto the end of the myArray variable.
+*/
+
+// Налаштування
+const myArray = [["John", 23], ["cat", 2]];
+
+// Змініть код лише під цим рядком
+const myArray2 = myArray;
+myArray2.push(["dog", 3]);
