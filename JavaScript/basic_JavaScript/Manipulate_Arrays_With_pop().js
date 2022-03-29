e0c9ad0b8f8a51314a4513c5aa31824cca404e0b
Rodriguezz <rodriguezz4060@gmail.com>
2022-03-29 18:32:21 +0300

    использование функции вывода последнего значения массива .pop()

---
+++ JavaScript/basic_JavaScript/Manipulate_Arrays_With_pop().js
@@ -0,0 +1,9 @@
+/*
+Use the .pop() function to remove the last item from myArray and assign the popped
+off value to a new variable, removedFromMyArray.*/
+
+// Налаштування
+const myArray = [["John", 23], ["cat", 2]];
+
+// Змініть код лише під цим рядком
+const removedFromMyArray = myArray.pop();
