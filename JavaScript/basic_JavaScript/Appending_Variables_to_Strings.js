dd651f74ffaa81eb03582fa18c57c0ca31680ea9
Rodriguezz <rodriguezz4060@gmail.com>
2022-03-29 13:26:37 +0300

    использование пееменных с оператором +=

---
+++ JavaScript/basic_JavaScript/Appending_Variables_to_Strings.js
@@ -0,0 +1,7 @@
+/*
+Set someAdjective to a string of at least 3
+characters and append it to myStr using the += operator.*/
+
+const someAdjective = "Name";
+let myStr = "Learning to code is ";
+myStr += someAdjective;
