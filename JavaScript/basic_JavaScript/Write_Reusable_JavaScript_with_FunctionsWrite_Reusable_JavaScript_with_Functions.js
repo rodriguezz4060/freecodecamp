5a9c0e76899ca2d946e63fef9a995dea123c618e
Rodriguezz <rodriguezz4060@gmail.com>
2022-03-30 15:15:11 +0300

    создание создание функции и вызов функции

---
+++ JavaScript/basic_JavaScript/Write_Reusable_JavaScript_with_FunctionsWrite_Reusable_JavaScript_with_Functions.js
@@ -0,0 +1,10 @@
+/*
+    Create a function called reusableFunction which prints the string Hi World to the dev console.
+    Call the function.
+*/
+
+function reusableFunction() {
+    console.log('Hi World')
+}
+
+reusableFunction();
