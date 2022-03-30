22d396d8e0668026bc0e52d418c87f6ded9af51b
Rodriguezz <rodriguezz4060@gmail.com>
2022-03-30 18:49:35 +0300

    Демонстрация Булевых (Boolen) зачений

---
+++ JavaScript/basic_JavaScript/Understanding_Boolean_Values.js
@@ -0,0 +1,10 @@
+/*Modify the welcomeToBooleans function so that it returns true instead of
+false when the run button is clicked.*/
+
+function welcomeToBooleans() {
+    // Змініть код лише під цим рядком
+
+    return true; // Змініть цей рядок
+
+    // Змініть код лише над цим рядком
+}
