9d02e9eddc5f280635467ff61a9ae9ad9e25883b
Rodriguezz <rodriguezz4060@gmail.com>
2022-03-30 17:18:09 +0300

    Использование глобальных и локальных переменных

---
+++ JavaScript/basic_JavaScript/Global_vs_Local_Scope_in _unctions.js
@@ -0,0 +1,15 @@
+/*
+Add a local variable to myOutfit function to override the value of outerWear with the string sweater.
+*/
+
+// Налаштування
+const outerWear = "T-Shirt";
+
+function myOutfit() {
+    // Змініть код лише під цим рядком
+    const outerWear = "sweater";
+    // Змініть код лише над цим рядком
+    return outerWear;
+}
+
+myOutfit();
