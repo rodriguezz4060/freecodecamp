873f2f1a7e82223328b17d00b83b3baf896a6aa5
Rodriguezz <rodriguezz4060@gmail.com>
2022-03-30 17:28:31 +0300

    Использование функций без return

---
+++ JavaScript/basic_JavaScript/Understanding_Undefined_Value_returned from_a_Function.js
@@ -0,0 +1,18 @@
+/*Create a function addFive without any arguments.
+This function adds 5 to the sum variable, but its returned value is undefined.*/
+
+// Налаштування
+let sum = 0;
+
+function addThree() {
+    sum = sum + 3;
+}
+
+// Змініть код лише під цим рядком
+function addFive() {
+    sum = sum + 5;
+}
+// Змініть код лише над цим рядком
+
+addThree();
+addFive();
