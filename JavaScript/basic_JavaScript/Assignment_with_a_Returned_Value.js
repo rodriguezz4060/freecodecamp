7e6b246144215a67b20baf450eef62d0ba113b8e
Rodriguezz <rodriguezz4060@gmail.com>
2022-03-30 17:44:41 +0300

    присвоение переменной

---
+++ JavaScript/basic_JavaScript/Assignment_with_a_Returned_Value.js
@@ -0,0 +1,12 @@
+/*Call the processArg function with an argument of 7 and assign its return value to the variable processed.*/
+
+// Налаштування
+let processed = 0;
+
+function processArg(num) {
+    return (num + 3) / 5;
+}
+
+// Змініть код лише під цим рядком
+
+processed = processArg(7);
