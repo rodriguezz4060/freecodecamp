fdcd587ed3abb30f634382edc9669f0a3e52838d
Rodriguezz <rodriguezz4060@gmail.com>
2022-03-30 15:03:54 +0300

    создание многострочных масивов

---
+++ JavaScript/basic_JavaScript/Shopping_List.js
@@ -0,0 +1,16 @@
+/*
+Create a shopping list in the variable myList. The list should be a
+multi-dimensional array containing several sub-arrays.
+The first element in each sub-array should contain a string with the name
+of the item. The second element should be a number representing the quantity i.e.
+["Chocolate Bar", 15]
+There should be at least 5 sub-arrays in the list.
+*/
+
+const myList = [
+    ['icecream', 10],
+    ['water', 2],
+    ['chocolate', 11],
+    ['bread', 9],
+    ['cheez', 2]
+];
