43c85d1b415177095985f8bf65633aada5ba0dbb
Rodriguezz <rodriguezz4060@gmail.com>
2022-03-30 19:11:30 +0300

    Демонстрация Булевых (Boolen) зачений true and false

---
+++ JavaScript/basic_JavaScript/Use_Conditional_Logic_with_If_Statements.js
@@ -0,0 +1,17 @@
+/*Create an if statement inside the function to return Yes,
+that was true if the parameter wasThatTrue is true and return
+No, that was false otherwise.*/
+
+function trueOrFalse(wasThatTrue) {
+    // Змініть код лише під цим рядком
+    if (wasThatTrue) {
+        return 'Yes, that was true';
+    }
+    return 'No, that was false';
+
+    trueOrFalse(true);
+    trueOrFalse(false);
+
+    // Змініть код лише над цим рядком
+
+}
