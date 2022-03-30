45e7874b992be107ac4cbb394d607d8f3f36c873
Rodriguezz <rodriguezz4060@gmail.com>
2022-03-30 16:48:30 +0300

    использование команды return

---
+++ JavaScript/basic_JavaScript/Return_a_Value_from_a_Function_with_Return.js
@@ -0,0 +1,11 @@
+/*
+Create a function timesFive that accepts one argument, multiplies it by 5, and returns the new value.*/
+
+function timesFive (num) {
+    return num * 5;
+}
+
+const answer1 = timesFive(0);
+const answer2 = timesFive(2);
+const answer3 = timesFive(5);
+
