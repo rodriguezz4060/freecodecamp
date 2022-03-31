f6dabd913655a816633f940c6b6c34cd1da1ffe5
Rodriguezz <rodriguezz4060@gmail.com>
2022-03-31 19:45:46 +0300

    логические функции

---
+++ JavaScript/basic_JavaScript/Returning_Boolean_Values_from_Functions.js
@@ -0,0 +1,9 @@
+/*Fix the function isLess to remove the if/else statements.*/
+
+function isLess(a, b) {
+    // Змініть код лише під цим рядком
+    return a < b;
+    // Змініть код лише над цим рядком
+}
+
+isLess(10, 15);
