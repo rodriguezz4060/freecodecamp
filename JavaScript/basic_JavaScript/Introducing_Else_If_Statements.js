5611f69e4ca333f9d6521bcb19b3ea8ea58ba07e
Rodriguezz <rodriguezz4060@gmail.com>
2022-03-31 09:28:01 +0300

    Условия else if

---
+++ JavaScript/basic_JavaScript/Introducing_Else_If_Statements.js
@@ -0,0 +1,15 @@
+/*Convert the logic to use else if statements.*/
+
+function testElseIf(val) {
+    if (val > 10) {
+        return "Greater than 10";
+    }
+
+    if (val < 5) {
+        return "Smaller than 5";
+    }
+
+    return "Between 5 and 10";
+}
+
+testElseIf(7);
