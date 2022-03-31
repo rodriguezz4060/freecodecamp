bbad72729ae84ff317c27a5999431fe33a1fe5e1
Rodriguezz <rodriguezz4060@gmail.com>
2022-03-31 18:28:03 +0300

    использование комады case с несколькими параметрами

---
+++ JavaScript/basic_JavaScript/Multiple_Identical_Options_in_Switch_Statements.js
@@ -0,0 +1,33 @@
+/*
+Write a switch statement to set answer for the following ranges:
+1-3 - Low
+4-6 - Mid
+7-9 - High
+Note: You will need to have a case statement for each number in the range.
+*/
+
+function sequentialSizes(val) {
+    let answer = "";
+    // Змініть код лише під цим рядком
+    switch (val) {
+        case 1:
+        case 2:
+        case 3:
+            answer = 'Low';
+            break;
+        case 4:
+        case 5:
+        case 6:
+            answer = 'Mid';
+            break;
+        case 7:
+        case 8:
+        case 9:
+            answer = 'High';
+            break;
+    }
+    // Змініть код лише над цим рядком
+    return answer;
+}
+
+sequentialSizes(1);
