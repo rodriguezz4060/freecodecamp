6f8c19977b12f1d1360d9059f84b174e65b60095
Rodriguezz <rodriguezz4060@gmail.com>
2022-04-01 16:31:19 +0300

    изменение свойств обьекта

---
+++ JavaScript/basic_JavaScript/Updating_Object_Properties.js
@@ -0,0 +1,15 @@
+/*Update the myDog object's name property.
+Let's change her name from Coder to Happy Coder.
+You can use either dot or bracket notation.*/
+
+// Налаштування
+const myDog = {
+    "name": "Coder",
+    "legs": 4,
+    "tails": 1,
+    "friends": ["freeCodeCamp Campers"]
+};
+
+// Змініть код лише під цим рядком
+
+myDog.name = 'Happy Coder';
