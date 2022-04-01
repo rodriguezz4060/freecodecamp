71cea4623365b8b8ea9a6f7930b344f092ac92f8
Rodriguezz <rodriguezz4060@gmail.com>
2022-04-01 10:17:31 +0300

    обращение к свойствам обьекта через точку (.)

---
+++ JavaScript/basic_JavaScript/Accessing_Object_Properties_with_Dot_Notation.js
@@ -0,0 +1,13 @@
+/*Read in the property values of testObj using dot notation. Set the variable hatValue
+equal to the object's property hat and set the variable shirtValue equal to the object's property shirt.*/
+
+// Налаштування
+const testObj = {
+    "hat": "ballcap",
+    "shirt": "jersey",
+    "shoes": "cleats"
+};
+
+// Змініть код лише під цим рядком
+const hatValue = testObj.hat;      // Змініть цей рядок
+const shirtValue = testObj.shirt;    // Змініть цей рядок
