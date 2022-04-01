dfd1f905358ac64219f727dc1004e7eb7f59ef97
Rodriguezz <rodriguezz4060@gmail.com>
2022-04-01 10:07:05 +0300

    создание обьектов

---
+++ JavaScript/basic_JavaScript/Build_JavaScript_Objects.js
@@ -0,0 +1,16 @@
+/*
+Make an object that represents a dog called myDog which contains
+the properties name (a string), legs, tails and friends.
+You can set these object properties to whatever values you want,
+as long as name is a string, legs and tails are numbers, and friends is an array.
+*/
+
+const myDog = {
+    // Змініть код лише під цим рядком
+    'name': 'Pes',
+    legs: 4,
+    tails: 1,
+    friends:['Humans', 'cat']
+
+    // Змініть код лише над цим рядком
+};
