e0de8a443b80d2b5cee57eb66c8775eae002a8c1
Rodriguezz <rodriguezz4060@gmail.com>
2022-04-01 16:19:25 +0300

    обращение к переменным через квадратные скобки ([])

---
+++ JavaScript/basic_JavaScript/Accessing_Object_Properties_with_Variables.js
@@ -0,0 +1,13 @@
+/*Set the playerNumber variable to 16. Then,
+use the variable to look up the player's name and assign it to player.*/
+
+// Налаштування
+const testObj = {
+    12: "Namath",
+    16: "Montana",
+    19: "Unitas"
+};
+
+// Змініть код лише під цим рядком
+const playerNumber = 16;  // Змініть цей рядок
+const player = testObj[playerNumber];   // Змініть цей рядок
