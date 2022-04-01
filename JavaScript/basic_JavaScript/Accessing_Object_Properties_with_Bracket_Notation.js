e479353ea494b9266b1cfb0d44051a69a6d99345
Rodriguezz <rodriguezz4060@gmail.com>
2022-04-01 13:27:50 +0300

    обращение к свойствам обьекта через квадратные скобки ([])

---
+++ JavaScript/basic_JavaScript/Accessing_Object_Properties_with_Bracket_Notation.js
@@ -0,0 +1,13 @@
+/*Read the values of the properties an entree and the drink of testObj using
+bracket notation and assign them to entreeValue and drinkValue respectively.*/
+
+// Налаштування
+const testObj = {
+    "an entree": "hamburger",
+    "my side": "veggies",
+    "the drink": "water"
+};
+
+// Змініть код лише під цим рядком
+const entreeValue = testObj["an entree"];   // Змініть цей рядок
+const drinkValue = testObj["the drink"];    // Змініть цей рядок
