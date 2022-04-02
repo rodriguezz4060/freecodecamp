9bae69a1ffa8b7f1e4b6d3023d4eb27f51c1b467
Rodriguezz <rodriguezz4060@gmail.com>
2022-04-02 13:02:10 +0300

    Использование цикла for и непарных чисел в порядке убывания

---
+++ JavaScript/basic_JavaScript/Count_Backwards_With_a_For_Loop.js
@@ -0,0 +1,10 @@
+/*Push the odd numbers from 9 through 1 to myArray using a for loop.*/
+
+// Налаштування
+const myArray = [];
+
+for (let i = 9; i > 0; i -= 2) {
+    myArray.push(i)
+}
+
+// Змініть код лише під цим рядком
