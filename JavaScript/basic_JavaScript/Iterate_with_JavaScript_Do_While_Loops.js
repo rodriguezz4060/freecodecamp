bc437bb3b1197c86823717cda46afcec41a32a69
Rodriguezz <rodriguezz4060@gmail.com>
2022-04-02 15:00:27 +0300

    использование цикла do...while

---
+++ JavaScript/basic_JavaScript/Iterate_with_JavaScript_Do_While_Loops.js
@@ -0,0 +1,12 @@
+/*Change the while loop in the code to a do...while loop so the loop will push only the number 10 to
+myArray, and i will be equal to 11 when your code has finished running.*/
+
+// Налаштування
+const myArray = [];
+let i = 10;
+
+// Змініть код лише під цим рядком
+do {
+    myArray.push(i);
+    i++
+} while (i < 5);
