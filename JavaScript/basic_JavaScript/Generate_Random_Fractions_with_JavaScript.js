0d4e46f627511dd047c66c3fc1558fed44de86fb
Rodriguezz <rodriguezz4060@gmail.com>
2022-04-02 16:48:20 +0300

    использование Math.random()

---
+++ JavaScript/basic_JavaScript/Generate_Random_Fractions_with_JavaScript.js
@@ -0,0 +1,10 @@
+/*Change randomFraction to return a random number instead of returning 0.*/
+
+function randomFraction() {
+
+    // Змініть код лише під цим рядком
+
+    return Math.random();
+
+    // Змініть код лише над цим рядком
+}
