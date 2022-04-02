adf321588bbeb51c8d40be372dc33d2c829a1d0a
Rodriguezz <rodriguezz4060@gmail.com>
2022-04-02 16:59:31 +0300

    использование Math.random() и целых чисел

---
+++ JavaScript/basic_JavaScript/Generate_Random_Whole_Numbers_with_JavaScript.js
@@ -0,0 +1,8 @@
+/*Use this technique to generate and return a random whole number between 0 and 9.*/
+
+function randomWholeNum() {
+
+    // Змініть код лише під цим рядком
+
+    return Math.floor(Math.random()*10);
+}
