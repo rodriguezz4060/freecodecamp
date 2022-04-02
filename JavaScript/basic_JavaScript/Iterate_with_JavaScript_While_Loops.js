6ccd94783e65ab40693403ef514b3a8532599269
Rodriguezz <rodriguezz4060@gmail.com>
2022-04-02 10:20:57 +0300

    Использование цикла while

---
+++ JavaScript/basic_JavaScript/Iterate_with_JavaScript_While_Loops.js
@@ -0,0 +1,11 @@
+/*Add the numbers 5 through 0 (inclusive) in descending order to myArray using a while loop.*/
+
+// Налаштування
+const myArray = [];
+let i = 5;
+
+while (i >= 0) {
+    myArray.push(i);
+    i--;
+}
+// Змініть код лише під цим рядком
