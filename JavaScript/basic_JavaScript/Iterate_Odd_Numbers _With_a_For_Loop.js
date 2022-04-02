578094ad718d4363d34bf70e6f7b6a76969b6dbe
Rodriguezz <rodriguezz4060@gmail.com>
2022-04-02 12:50:10 +0300

    Использование цикла for и непарных чисел

---
+++ JavaScript/basic_JavaScript/Iterate_Odd_Numbers _With_a_For_Loop.js
@@ -0,0 +1,9 @@
+/*Push the odd numbers from 1 through 9 to myArray using a for loop*/
+
+// Налаштування
+const myArray = [];
+
+for (let i=1; i <= 9; i += 2){
+    myArray.push(i)
+}
+// Змініть код лише під цим рядком
