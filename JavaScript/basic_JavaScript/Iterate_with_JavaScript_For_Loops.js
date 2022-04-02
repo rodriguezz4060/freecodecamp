33ec52b0c5bfbd1bdc621b96cd499062eaefd4b9
Rodriguezz <rodriguezz4060@gmail.com>
2022-04-02 12:39:45 +0300

    Использование цикла for

---
+++ JavaScript/basic_JavaScript/Iterate_with_JavaScript_For_Loops.js
@@ -0,0 +1,10 @@
+/*Use a for loop to push the values 1 through 5 onto myArray.*/
+
+// Налаштування
+const myArray = [];
+
+// Змініть код лише під цим рядком
+
+for (let i=1; i <= 5; i++) {
+    myArray.push(i);
+}
